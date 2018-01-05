4f9890186dc64e9e4573ba247636d4a66b3401a3
MJ <mdjacksonvideo@gmail.com>
2018-01-05 10:26:37 -0800

    pass third test: getFood.

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -2,13 +2,18 @@ package models;
 
 public class Event {
     private int guests;
+    private int food;
 
-    public Event(int guests) {
+    public Event(int guests, int food) {
         this.guests = guests;
+        this.food = food;
 
 
     }
     public int getGuests(){
         return this.guests;
+    }
+    public int getFood(){
+        return this.food;
     }
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -6,12 +6,12 @@ public class EventTest {
 
     @Test
     public void runEvent_instanceWorks_2() {
-        Event testEvent = new Event(2);
+        Event testEvent = new Event(2,4);
         assertEquals(true, testEvent instanceof Event);
     }
     @Test
     public void runEvent_getsGuests_2() {
-        Event testEvent = new Event(2);
+        Event testEvent = new Event(2,4);
         assertEquals(2, testEvent.getGuests());
     }
     @Test
