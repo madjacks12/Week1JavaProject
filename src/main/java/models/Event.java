3d6a8d61e91a0add7f032e2a36e2277bbf2586b3
MJ <mdjacksonvideo@gmail.com>
2018-01-05 12:02:02 -0800

    changed food property from int to string to present text choices of meals.

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -2,12 +2,12 @@ package models;
 
 public class Event {
     private int guests;
-    private int food;
+    private String food;
     private int bev;
     private int ent;
     private int price;
 
-    public Event(int guests, int food, int bev, int ent, int price) {
+    public Event(int guests, String food, int bev, int ent, int price) {
         this.guests = guests;
         this.food = food;
         this.bev = bev;
@@ -32,10 +32,25 @@ public class Event {
         return this.price;
     }
 
+    public int foodPrice() {
+        if (15 > this.guests) {
+            this.price += 100;
+        } else if ((this.guests >= 15) && (this.guests <= 50)) {
+            this.price += 200;
+        } else if ((this.guests > 50) && (this.guests <= 100)) {
+            this.price += 300;
+        } else if ((this.guests > 100) && (this.guests <= 300)) {
+            this.price += 300;
+        }
+        else { return 0;
+        }
+        return this.price;
+    }
+
     public int getGuests(){
         return this.guests;
     }
-    public int getFood(){
+    public String getFood(){
         return this.food;
     }
     public int getBev(){
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -6,45 +6,39 @@ public class EventTest {
 
     @Test
     public void runEvent_instanceWorks_2() {
-        Event testEvent = new Event(2,4,6,8,10);
+        Event testEvent = new Event(2,"chicken",6,8,10);
         assertEquals(true, testEvent instanceof Event);
     }
     @Test
     public void runEvent_getsGuests_2() {
-        Event testEvent = new Event(2,4,6,8,10);
+        Event testEvent = new Event(2,"chicken",6,8,10);
         assertEquals(2, testEvent.getGuests());
     }
     @Test
     public void runEvent_getsFood_4() {
-        Event testEvent = new Event(2, 4,6,8,10);
-        assertEquals(4, testEvent.getFood());
+        Event testEvent = new Event(2, "chicken",6,8,10);
+        assertEquals("chicken", testEvent.getFood());
     }
     @Test
     public void runEvent_getsBeverages_6() {
-        Event testEvent = new Event(2, 4, 6,8,10);
+        Event testEvent = new Event(2, "chicken", 6,8,10);
         assertEquals(6,  testEvent.getBev());
     }
     @Test
     public void runEvent_getsEnt_8() {
-        Event testEvent = new Event(2, 4, 6, 8,10);
+        Event testEvent = new Event(2, "chicken", 6, 8,10);
         assertEquals(8,  testEvent.getEnt());
     }
     @Test
     public void runEvent_getsPrice_10() {
-        Event testEvent = new Event(2, 4, 6, 8, 10);
+        Event testEvent = new Event(2, "chicken", 6, 8, 10);
         assertEquals(10,  testEvent.getPrice());
     }
     @Test
     public void runEvent_guestsReturnsPrice_300() {
-        Event testEvent = new Event(200, 4, 6, 8, 0);
+        Event testEvent = new Event(200, "chicken", 6, 8, 0);
 
         assertEquals(300,  testEvent.guestPrice());
-    }
-    @Test
-    public void runEvent_foodReturnsPrice_200() {
-        Event testEvent = new Event(200, 4, 6, 8, 0);
-
-        assertEquals(200,  testEvent.foodPrice());
     }
 
 }
