8257bf8e86a324b0c4dcf18b32020661cba7b66b
MJ <mdjacksonvideo@gmail.com>
2018-01-05 12:25:43 -0800

    switched over beverage and entertainment to Strings and added seperate guestPrice, foodPrice, entPrice properties to the Event method, because I realized I won't be able to return a price integer in a String formula.

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -3,49 +3,41 @@ package models;
 public class Event {
     private int guests;
     private String food;
-    private int bev;
-    private int ent;
-    private int price;
+    private String bev;
+    private String ent;
+    private int guestPrice;
+    private String foodPrice;
+    private String bevPrice;
+    private String entPrice;
 
-    public Event(int guests, String food, int bev, int ent, int price) {
+    public Event(int guests, String food, String bev, String ent, int guestPrice, String foodPrice, String bevPrice, String entPrice) {
         this.guests = guests;
         this.food = food;
         this.bev = bev;
         this.ent = ent;
-        this.price = price;
+        this.guestPrice = guestPrice;
+        this.foodPrice = foodPrice;
+        this.entPrice = entPrice;
     }
 
 
 
     public int guestPrice() {
         if (15 > this.guests) {
-            this.price += 100;
+            this.guestPrice += 100;
         } else if ((this.guests >= 15) && (this.guests <= 50)) {
-            this.price += 200;
+            this.guestPrice += 200;
         } else if ((this.guests > 50) && (this.guests <= 100)) {
-            this.price += 300;
+            this.guestPrice += 300;
         } else if ((this.guests > 100) && (this.guests <= 300)) {
-            this.price += 300;
+            this.guestPrice += 300;
         }
         else { return 0;
         }
-        return this.price;
+        return this.guestPrice;
     }
 
-    public int foodPrice() {
-        if (15 > this.guests) {
-            this.price += 100;
-        } else if ((this.guests >= 15) && (this.guests <= 50)) {
-            this.price += 200;
-        } else if ((this.guests > 50) && (this.guests <= 100)) {
-            this.price += 300;
-        } else if ((this.guests > 100) && (this.guests <= 300)) {
-            this.price += 300;
-        }
-        else { return 0;
-        }
-        return this.price;
-    }
+
 
     public int getGuests(){
         return this.guests;
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -6,37 +6,37 @@ public class EventTest {
 
     @Test
     public void runEvent_instanceWorks_2() {
-        Event testEvent = new Event(2,"chicken",6,8,10);
+        Event testEvent = new Event(2,"chicken","soda","band",10, "10", "20", "30");
         assertEquals(true, testEvent instanceof Event);
     }
     @Test
     public void runEvent_getsGuests_2() {
-        Event testEvent = new Event(2,"chicken",6,8,10);
+        Event testEvent = new Event(2,"chicken","soda","band",10, "10", "20", "30");
         assertEquals(2, testEvent.getGuests());
     }
     @Test
     public void runEvent_getsFood_4() {
-        Event testEvent = new Event(2, "chicken",6,8,10);
+        Event testEvent = new Event(2,"chicken","soda","band",10, "10", "20", "30");
         assertEquals("chicken", testEvent.getFood());
     }
     @Test
     public void runEvent_getsBeverages_6() {
-        Event testEvent = new Event(2, "chicken", 6,8,10);
+        Event testEvent = new Event(2,"chicken","soda","band",10, "10", "20", "30");
         assertEquals(6,  testEvent.getBev());
     }
     @Test
     public void runEvent_getsEnt_8() {
-        Event testEvent = new Event(2, "chicken", 6, 8,10);
+        Event testEvent = new Event(2,"chicken","soda","band",10, "10", "20", "30");
         assertEquals(8,  testEvent.getEnt());
     }
     @Test
     public void runEvent_getsPrice_10() {
-        Event testEvent = new Event(2, "chicken", 6, 8, 10);
+        Event testEvent = new Event(2,"chicken","soda","band",10, "10", "20", "30");
         assertEquals(10,  testEvent.getPrice());
     }
     @Test
     public void runEvent_guestsReturnsPrice_300() {
-        Event testEvent = new Event(200, "chicken", 6, 8, 0);
+        Event testEvent = new Event(2,"chicken","soda","band",10, "10", "20", "30");
 
         assertEquals(300,  testEvent.guestPrice());
     }
