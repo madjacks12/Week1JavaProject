6a10c35db9dbd44d376ef6e1b219f68f19014417
MJ <mdjacksonvideo@gmail.com>
2018-01-05 10:04:12 -0800

    initial commit

---
+++ src/main/java/models/Event.java
@@ -0,0 +1,8 @@
+package models;
+
+public class Event {
+
+    private string
+
+    public Game()
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,9 @@
+package models;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class Test {
+
+    }
+}
+
