28ed58b4e6ee056d1612dcb104bc381721351e8a
MJ <mdjacksonvideo@gmail.com>
2018-01-05 10:24:25 -0800

    fail third tesst: getFood.

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -5,14 +5,19 @@ import static org.junit.Assert.*;
 public class EventTest {
 
     @Test
-    public void runEvent_instanceWorks() {
+    public void runEvent_instanceWorks_2() {
         Event testEvent = new Event(2);
         assertEquals(true, testEvent instanceof Event);
     }
     @Test
-    public void runEvent_getsGuests() {
+    public void runEvent_getsGuests_2() {
         Event testEvent = new Event(2);
         assertEquals(2, testEvent.getGuests());
+    }
+    @Test
+    public void runEvent_getsFood_4() {
+        Event testEvent = new Event(2, 4);
+        assertEquals(2, 4, testEvent.getGuests());
     }
 
 }
